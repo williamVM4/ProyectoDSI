05b91a055e27647e86cccbe9f396fdc5800f588a
AngelRdz <ar19085@ues.edu.sv>
2022-06-08 13:01:03 -0600

    formulario prima

--- SistemaDeRegistroDePagos/apps/facturacion/forms.py
+++ SistemaDeRegistroDePagos/apps/facturacion/forms.py
@@ -1,26 +1,43 @@
 from cProfile import label
 from dataclasses import fields
+from statistics import mode
 from django.forms import ModelForm
-from .models import pagoMantenimiento, prima
+from .models import *
+from apps.inventario.models import lote
 
-"""class agregarPrimaForm(ModelForm):
+class agregarPrimaForm(ModelForm):
     class Meta:
         model = prima
-        fields = ('numeroReciboPrima','fechaPrima','montoPrima','conceptoPrima','tipoPago','referencia','cuentaBancaria',)
+        fields = ('numeroReciboPrima','conceptoPrima',)
         label= {
             'numeroReciboPrima':('Numero de Recibo de la Prima: '),
-            'fechaPrima': ('Fecha Prima'),
-            'montoPrima': ('Monto Prima'),
             'conceptoPrima': ('Concepto Prima'),
         }
         help_texts = {
             'numeroReciboPrima':('Campo Obligatorio'),
-            'fechaPrima': ('Campo Obligatorio'),
-            'montoPrima': ('Campo Obligatorio'),
             'conceptoPrima': ('Campo Obligatorio'),
         }
 
-class agregarPagoMantenimientoForm(ModelForm):
+
+
+class pago(ModelForm):
+    class Meta:
+        model = pago
+        fields = {'monto','tipoPago','referencia','fechaPago'}
+        label = {
+            'monto':(''),
+            'tipoPago':(''),
+            'referencia':(''),
+            'fechaPago':(''),
+        }
+        help_texts = {
+            'monto':(''),
+            'tipoPago':(''),
+            'referencia':(''),
+            'fechaPago':(''),
+        }
+
+"""class agregarPagoMantenimientoForm(ModelForm):
     class Meta:
         model = pagoMantenimiento
         fields = ('numeroReciboMantenimiento','fechaPago','monto','conceptoOtros','montoOtros')"""
