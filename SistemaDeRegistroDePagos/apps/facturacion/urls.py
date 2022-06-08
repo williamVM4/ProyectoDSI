a1f5d7b68663fd6fdf0a6abdea24df5be440290c
AngelRdz <ar19085@ues.edu.sv>
2022-06-08 09:50:10 -0600

    primas

--- SistemaDeRegistroDePagos/apps/facturacion/urls.py
+++ SistemaDeRegistroDePagos/apps/facturacion/urls.py
@@ -4,7 +4,6 @@ from apps.facturacion.views import *
 
 urlpatterns = [
     path('caja/<str:idp>/',caja.as_view(),name='caja'),
-    #path('agregarPagoMantenimiento/<str:idp>/',agregarPagoMantenimiento.as_view(),name='agregarPagoMantenimineto'),
-    path('agregarPrima/<str:idp>/',agregarPrima.as_view(),name='agregarPrima'),
+
 
 ]
