96ab32343317e21edf4d2b090d3e577573ab4327
williamVM4 <enriquemancia25@gmail.com>
2022-06-09 12:13:05 -0600

    listar pagos caja

--- SistemaDeRegistroDePagos/apps/facturacion/models.py
+++ SistemaDeRegistroDePagos/apps/facturacion/models.py
@@ -1,6 +1,6 @@
 from math import fabs
 from django.db import models
-from django.conf import settings
+from django.contrib.auth.models import User
 from crum import get_current_user
 
 pago_tipo = [
@@ -17,8 +17,7 @@ class pago(models.Model):
         null=False, blank=False,
         choices=pago_tipo
     )
-
-    referencia = models.CharField(max_length=100, blank=True, null = True)
+    referencia = models.CharField(max_length=100, blank=True, default="")
     cuentaBancaria = models.ForeignKey('inventario.cuentaBancaria',blank=True, null = True, on_delete=models.CASCADE)
     fechaPago = models.DateField()
     
@@ -26,7 +25,7 @@ class pago(models.Model):
 class prima(models.Model):
     numeroReciboPrima = models.CharField(max_length=10, primary_key=True)
     detalleVenta = models.ForeignKey('inventario.detalleVenta',blank=True, on_delete=models.CASCADE)
-    usuarioCreacion = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,blank=True, null = True, related_name='user_creation_prima')
+    usuarioCreacion = models.ForeignKey(User,on_delete=models.CASCADE,blank=True, null = True, related_name='user_creation_prima')
     conceptoPrima = models.CharField(max_length=50)
 
     def __str__(self):
@@ -40,8 +39,8 @@ class prima(models.Model):
         super(prima,self).save()
 
 class BasePagoModel(models.Model):
-    conceptoOtros = models.CharField(max_length=100,blank=True)
-    montoOtros = models.FloatField(blank=True)
+    conceptoOtros = models.CharField(max_length=100,blank=True, default="")
+    montoOtros = models.FloatField(blank=True,default=0)
     numeroCuotaEstadoCuenta = models.ForeignKey('monitoreo.cuotaEstadoCuenta',blank=True, on_delete=models.CASCADE)
     
     class Meta:
@@ -49,7 +48,7 @@ class BasePagoModel(models.Model):
 
 class pagoFinanciamiento(BasePagoModel):
     numeroReciboFinanciamiento = models.CharField(max_length=30, primary_key=True)
-    usuarioCreacion = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,blank=True, null = True, related_name='user_creation_financiamiento')
+    usuarioCreacion = models.ForeignKey(User,on_delete=models.CASCADE,blank=True, null = True, related_name='user_creation_financiamiento')
     mantenimiento = models.FloatField()
     comision = models.FloatField()
     
@@ -65,17 +64,11 @@ class pagoFinanciamiento(BasePagoModel):
 
 class pagoMantenimiento(BasePagoModel):
     numeroReciboMantenimiento = models.CharField(max_length=30, primary_key=True)
-    usuarioCreacion = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,blank=True, null = True, related_name='user_creation_mantenimiento')
+    usuarioCreacion = models.ForeignKey(User,on_delete=models.CASCADE,blank=True, null = True, related_name='user_creation_mantenimiento')
 
     def __str__(self):
         return self.numeroReciboMantenimiento
 
-    def save(self, force_insert=False, force_update=False,using=None, update_fields=None):
-        user = get_current_user()
-        if user is not None:
-            if not self.pk:
-                self.user_creation_mantenimiento = user
-        super(pagoMantenimiento,self).save()
 
 
 
